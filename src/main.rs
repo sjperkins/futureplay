ee610d0edce37bd4b102159f5ca485f03e18fb32
Simon Perkins <simon.perkins@gmail.com>
2017-01-02 19:55:05 +0200

    Rework code to be more rustlike
    - Create an iterator over work arguments, mapping them to CPU futures
      spawned on the cpu pool.
    - Work function now returns a struct containing useful information on
      the work completed.
    - Above iterator is converted to an unordered future stream.
    - Handle failed futures through the stream.or_else() method
    - Handle successful futures as they complete through the
      stream.for_each() method

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,76 @@
+extern crate chrono;
 extern crate futures;
 extern crate futures_cpupool;
 
-use futures::{Future, BoxFuture, Stream};
-use futures_cpupool::CpuPool;
+use std::thread;
 
-fn run(n: u64) -> BoxFuture<u64, ()> {
-    let res = (0..n).fold(0, |sum, x| sum + x);
-    futures::done(Ok(res)).boxed()
+use chrono::{DateTime, UTC};
+use futures::{Future, BoxFuture, Stream};
+
+struct WorkResult {
+    pub n: u64,
+    pub thread: thread::Thread,
+    pub start_time: DateTime<UTC>,
+    pub sum: u64,
+}
+
+fn work(n: u64) -> BoxFuture<WorkResult, u64> {
+    let result = match n {
+        // Fail on 1000 to create an or_else() case on the stream
+        1000 => Err(n),
+        // Default case
+        _ => {
+            Ok(WorkResult {
+                n: n,
+                thread: thread::current(),
+                start_time: UTC::now(),
+                sum: (0..n).fold(0, |sum, x| sum + x)
+            })
+        }
+    };
+
+    // Returned a boxed future of the result
+    futures::done(result).boxed()
 }
 
 fn main() {
-    let pool = CpuPool::new(4);
-    // let a = pool.spawn_fn(move || { run(100000) });
-    // let b = pool.spawn_fn(move || { run(10000) });
-    // let c = pool.spawn_fn(move || { run(1000) });
-    let a = pool.spawn(run(100000));
-    let b = pool.spawn(run(10000));
-    let c = pool.spawn(run(1000));
+    // Create a thread pool with thread name prefixes
+    // alternatively, could use:
+    //let pool = futures_cpupool::CpuPool::new_num_cpus();
+    let pool = futures_cpupool::Builder::new()
+        .name_prefix("pool_thread_")
+        .create();
 
-    let futures = vec!(a,b,c);
-    let stream = futures::stream::futures_unordered(futures.into_iter());
+    // Arguments for our work function
+    let args = vec!(1000000, 100000, 10000, 1000);
 
-    let iterate = stream.for_each(|v| {
-        println!("Value {}", v);
-        Ok(())
-    });
+    let iterate = args.into_iter()
+        // pool.spawn_fn creates a lazy future for the cpu pool to execute
+        // shorthand for pool.spawn(futures::lazy(move || work(v)))
+        .map(|v| pool.spawn_fn(move || work(v)));
 
-    iterate.wait().unwrap();
+    // Iterate over the cpu futures, printing results
+    let stream = futures::stream::futures_unordered(iterate)
+        // Handle the error case in work
+        .or_else(|e| pool.spawn_fn(move || work(e+1)))
+        // Handle result case of work
+        .for_each(|r| {
+            let finish_time = UTC::now();
+            println!("Argument     {}\n\
+                      Result       {}\n\
+                      Ran on thead {}\n\
+                      Started at   {}\n\
+                      Completed at {}\n\
+                      Duration     {}\n",
+                        r.n,
+                        r.sum,
+                        r.thread.name().unwrap(),
+                        r.start_time.format("%H:%M:%S%.6f"),
+                        finish_time.format("%H:%M:%S%.6f"),
+                        finish_time - r.start_time);
+            Ok(())
+        });
 
-    // This collects all futures into a vecto
-    // let result = stream.collect().wait().unwrap();
-
-    // for r in result {
-    //     println!("result {:?}", r);
-    //
-    // }
-
+    // Execute the stream
+    stream.wait().unwrap();
 }
